f7943fd459e55e1ba82e6ae2f3cf5e3cc6cde0c7
N. Escobar <nick@nickesc.io>
2026-02-09 14:39:50 -0500

    updating app to use analyze button

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,31 @@
 import "./App.css";
+import {useState, useCallback} from "react";
 import Header from "./components/Header";
 import UrlInput from "./components/UrlInput";
 import VisualizationPanel from "./components/VisualizationPanel";
+import type {Keyword} from "./types";
 
 function App() {
+    const [url, setUrl] = useState("");
+    const [keywords, setKeywords] = useState<Keyword[]>([]);
+
+    const onAnalyze = useCallback(async (nextUrl: string) => {
+        try {
+            setUrl(nextUrl);
+            setKeywords([]);
+            console.log(nextUrl);
+        } catch (err) {
+            setKeywords([]);
+            console.error(err);
+        }
+    }, []);
+
     return (
         <>
             <Header />
             <main>
                 <VisualizationPanel />
-                <UrlInput />
+                <UrlInput onAnalyze={onAnalyze} />
             </main>
         </>
     );
