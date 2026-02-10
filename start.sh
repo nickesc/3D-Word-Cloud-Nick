1630f35c9c9604971073165c0b6fc6d49c232294
N. Escobar <nick@nickesc.io>
2026-02-09 22:53:02 -0500

    update start script

--- start.sh
+++ start.sh
@@ -4,11 +4,11 @@ pip3 install -r requirements.txt
 
 npm install --prefix app
 
-# start backend
-mkdir -p logs
-uvicorn server.main:app --port 8127 --reload >logs/backend.log 2>&1 &
-
 # start frontend
-npm run dev --prefix app
+mkdir -p logs
+npm run dev --prefix app -- >logs/frontend.log 2>&1 &
+
+# start backend
+uvicorn server.main:app --port 8127
 
 
